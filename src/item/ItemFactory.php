13eeed7dbbdae044602736ff3c884b238ee09e8b
dataweb <bert@data-web.be>
2021-03-18 08:31:35 +0100

    Little bug

--- src/item/ItemFactory.php
+++ src/item/ItemFactory.php
@@ -17,20 +17,16 @@ class ItemFactory {
    * @throws DwapiException
    */
   public static function create($entity_type = "", $logged_in_user = NULL) {
-    $to_check_classes = [];
-    if ($entity_type != "") {
-      $to_check_classes[] = "dwApiLib\\item\\".Project::getInstance()->type."\\".ucfirst(str_replace("-", "_", $entity_type));
-    }
-    $to_check_classes[] = "dwApiLib\\item\\".Project::getInstance()->type."\\Item";
-
     $api_class = get_class(DwApiLib::getInstance());
     $api_ns = substr($api_class, 0, strrpos($api_class, '\\'));
-    if ($api_ns != "dwApiLib") {
-      array_unshift($to_check_classes, $api_ns."\\item\\".Project::getInstance()->type."\\Item");
-      if ($entity_type != "") {
-        array_unshift($to_check_classes, $api_ns."\\item\\".Project::getInstance()->type."\\".ucfirst(str_replace("-", "_", $entity_type)));
-      }
+
+    $to_check_classes = [];
+    if ($entity_type != "") {
+      $to_check_classes[] = $api_ns."\\item\\".Project::getInstance()->type."\\".ucfirst(str_replace("-", "_", $entity_type));
+      $to_check_classes[] = "dwApiLib\\item\\".Project::getInstance()->type."\\".ucfirst(str_replace("-", "_", $entity_type));
     }
+    $to_check_classes[] = $api_ns."\\item\\".Project::getInstance()->type."\\Item";
+    $to_check_classes[] = "dwApiLib\\item\\".Project::getInstance()->type."\\Item";
 
     foreach ($to_check_classes as $class) {
       if (class_exists($class)) {
